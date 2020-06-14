75620646580df41ea3e7a5b4d440da2bf1d85e86
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-15 05:22:22 +0900

    feat: Added media settings

--- sns/settings.py
+++ sns/settings.py
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+# localhost/mediaで参照できるようにする
+MEDIA_URL = '/media/'
