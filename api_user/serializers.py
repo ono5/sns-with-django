c95c0c659fa9e846d2a35c5a344c9a15dc50a155
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-16 05:46:14 +0900

    feat: Added api_user serializer

---
+++ api_user/serializers.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from core.models import Profile, FriendRequest
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    created_on = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ('id', 'nick_name', 'user_pro', 'created_on', 'img')
+        extra_kwargs = {'user_pro': {'read_only': True}}
+
+
+class FriendRequestSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FriendRequest
+        fields = ('id', 'ask_from', 'ask_to', 'approved')
+        extra_kwargs = {'ask_from', {'read_only': True}}
